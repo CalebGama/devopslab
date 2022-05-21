2e05b2e9edf990e1e6019554ba02508d596396af
CalebGama <105398927+CalebGama@users.noreply.github.com>
2022-05-20 21:17:58 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "Hello World, Hi, how are you?"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run()
